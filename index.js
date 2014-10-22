eaa7a66aab842a582c9e2310dbf340d208df1467
Miguel Camba <miguel.camba@gmail.com>
2014-10-22 14:47:40 +0100

    Add yuidoc command

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 'use strict';
 
 module.exports = {
-  name: 'ember-cli-yuidoc'
+  name: 'ember-cli-yuidoc',
+  includedCommands: function() {
+    console.log('passing though includedCommands');
+    return {
+      'yuidoc': require('./lib/commands/yuidoc')
+    }
+  },
 };
