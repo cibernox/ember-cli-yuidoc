860e1a46f6857a069236079a1f663e227768299b
Miguel Camba <miguel.camba@gmail.com>
2014-10-23 01:53:16 +0100

    Added generator yo create yuidoc.json manifest

---
+++ blueprints/yuidoc/index.js
@@ -0,0 +1,26 @@
+module.exports = {
+  description: 'Generates yuidoc.json manifest',
+  normalizeEntityName: function() {},
+  afterInstall: function() {
+    var fs = require('fs');
+    var pkginfo = this.project.pkg;
+    var config = {
+      name: pkginfo.name,
+      description: pkginfo.description,
+      version: pkginfo.version,
+      options: {
+        paths: [],
+        exclude: "vendor",
+        outdir: "docs"
+      }
+    };
+    if (pkginfo.keywords.indexOf('ember-addon') !== -1){
+      console.log("Generating yuidoc.json for ember-addon named " + config.name);
+      config.options.paths.push("addon");
+    } else {
+      console.log("Generating yuidoc.json for app named " + config.name);
+      config.options.paths.push("app");
+    }
+    fs.writeFileSync('yuidoc.json', JSON.stringify(config, null, 2));
+  }
+};
