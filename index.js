3aea10405c5cce07faf0ebb1530200b313c9684e
Miguel Camba <miguel.camba@gmail.com>
2014-10-30 20:15:40 +0000

    If /addon exists, watch it. If not, watch /app

--- index.js
+++ index.js
@@ -2,12 +2,15 @@
 
 var YuidocCompiler = require('broccoli-yuidoc');
 var mergeTrees = require('broccoli-merge-trees');
+var fs = require('fs');
 
 module.exports = {
   name: 'ember-cli-yuidoc',
 
-  postprocessTree: function(type, workingTree){
-    var yuidocTree = new YuidocCompiler('addon', {
+  postprocessTree: function(type, workingTree) {
+    var codeFolder = fs.existsSync('addon') ? 'addon' : 'app';
+
+    var yuidocTree = new YuidocCompiler(codeFolder, {
       srcDir: '/',
       destDir: '/docs',
       yuidoc: {}
